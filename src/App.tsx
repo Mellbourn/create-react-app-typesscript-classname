7d3ad70e7b09d02b49c67421d06d8336404ea010
Klas Mellbourn <klas@mellbourn.net>
2020-03-28 13:31:15 +0100

    add classnames usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import classNames from "classnames";
 
 function App() {
+  const Foo = classNames({ a: true, b: "other" });
+  console.log("Foo", Foo);
   return (
     <div className="App">
       <header className="App-header">
